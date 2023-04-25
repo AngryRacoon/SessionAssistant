ce9580a79280dee0f39dde21bc74f0d36861c058
AngryRacoon <03sidcrafter@gmail.com>
2023-04-25 23:45:26 +0300

    Add a handler for the navigation bar and the add button. Change color for navigation bar

--- app/src/main/java/com/example/sessionassistant/MainActivity.kt
+++ app/src/main/java/com/example/sessionassistant/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.sessionassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.item_1 -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.item_2 -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                else -> false
+            }
+        }
+        val button = findViewById<Button>(R.id.iconTonalButton)
+        button.setOnClickListener {
+            // Обработка нажатия на кнопку
+        }
     }
 }
+
